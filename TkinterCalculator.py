8d0d4e36d23024085261ae7d07dcca4760f064cf
mahirvahmad <63204860+mahirvahmad@users.noreply.github.com>
2020-07-12 13:56:53 +0300

    Icon file code disabled as

--- TkinterCalculator.py
+++ TkinterCalculator.py
@@ -10,7 +10,7 @@ CC = "#ffdead" #Chocolate Chip Cookies
 #Prepaing the ground for building the Calculator
 calc = Tk()
 calc.title("                                   +1-2%3√4/5* CALCULATOR *6/7√8%9-0+")
-calc.iconbitmap('calcico.ico')
+#calc.iconbitmap('calcico.ico')
 calc.config(bg="#2C001E")
 canvas=Canvas(calc,bg='#2C001E')
 canvas.pack(padx=15, pady=15)
